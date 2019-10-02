1760a6c0d8517d2937cbe1f43578e390ac6b77c7
Junxi Li <jimkeecn@gmail.com>
2019-10-02 15:15:37 +1000

    first migration to local VS DB

---
+++ Migrations/20191002044334_AddSimpleModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnetRestfulAPI.Migrations
+{
+    public partial class AddSimpleModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "simpleModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HowTo = table.Column<string>(nullable: true),
+                    PlatForm = table.Column<string>(nullable: true),
+                    CommandLine = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_simpleModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "simpleModel");
+        }
+    }
+}
