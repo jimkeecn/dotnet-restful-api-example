e2e700ce985d9fa48c535dce0fa2bbc5abad17e3
Junxi Li <jimkeecn@gmail.com>
2019-10-02 14:18:51 +1000

    init EntityFramework

---
+++ Models/Api.cs
@@ -0,0 +1,10 @@
+namespace dotnetRestfulAPI.Models
+{
+    public class simpleModel
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string PlatForm { get; set; }
+        public string CommandLine { get; set; }
+    }
+}
